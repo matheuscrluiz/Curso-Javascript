1e7884ecceaf44c1b53f2ce63e2a28ebdf78636a
matheuscrluiz <matheuscrluiz@gmail.com>
2023-12-31 17:18:24 -0300

    feat: add new clear items btn and add new alert for exceptions and sucess

--- aula67/assets/js/main.js
+++ aula67/assets/js/main.js
@@ -1,12 +1,67 @@
 const inputTarefa = document.querySelector('.input-tarefa') 
 const btnTarefa = document.querySelector('.btn-tarefa') 
+const btnDelete = document.querySelector('.btn-delete') 
 const tarefas = document.querySelector('.tarefas') 
+const alert = document.querySelector('.alert') 
+const alertSucess = document.querySelector('.alert-sucess') 
 
+alert.textContent = '';
+alert.classList.remove('alert');
+alertSucess.textContent = '';
+alertSucess.classList.remove('alert-sucess');
 // capturando evento do mouse
 btnTarefa.addEventListener('click', function() {
-  if(!inputTarefa.value) return; //nao captura os valores nulos
+  // if(!inputTarefa.value) return; //nao captura os valores nulos
 
+  if(!inputTarefa.value){
+    
+    alert.textContent = 'Insira uma tarefa para adicionar';
+    alert.classList.add('alert')
+
+    setTimeout(function() {
+      alert.textContent = '';
+      alert.classList.remove('alert');
+    }, 2000);
+    return
+  }
+  else{
+    alertSucess.textContent = 'Tarefa inserida com sucesso';
+    alertSucess.classList.add('alert-sucess')
+
+    setTimeout(function() {
+      alertSucess.textContent = '';
+      alertSucess.classList.remove('alert-sucess');
+    }, 2000);
+
+  }
   criaTarefa(inputTarefa.value);
+});
+
+// capturando evento do mouse de deletar
+btnDelete.addEventListener('click', function() {
+  
+  // selecionando a lsita de tarefas ul
+  const listaTarefas = document.querySelector('.tarefas');
+  
+  if(!listaTarefas.firstChild){
+
+    alert.textContent = 'Lista de tarefas vazia';
+    alert.classList.add('alert')
+
+    setTimeout(function() {
+      alert.textContent = '';
+      alert.classList.remove('alert');
+    }, 2000);
+  }
+ 
+  /* verifica se ainda há um primeiro filho dentro da lista 
+  "listaTarefas.firstChild" retorna o primeiro elemento filho 
+  (no caso, o primeiro <li> dentro do <ul>). */
+  while (listaTarefas.firstChild) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
+  salvarTarefas();
+  
 });
 
 
@@ -16,9 +71,12 @@ function criaBotaoApagar(li){
   botaoApagar.innerHTML = '<i class="fas fa-trash"></i>';
   botaoApagar.setAttribute('class', 'apagar');
   botaoApagar.setAttribute('title', 'Apagar tarefa'); //tooltip
-
+  
   li.appendChild(botaoApagar);
 };
+
+
+
 function criaTarefa(textoInput){
   const li = criaLi();
   li.innerText = textoInput;
@@ -62,6 +120,8 @@ document.addEventListener('click', function(e){
     salvarTarefas();
   }
 });
+
+
 
 function salvarTarefas(){
   
